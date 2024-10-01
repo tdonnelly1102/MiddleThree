110b4e8baf1072166f8b31901bc375fef1848232
tdonnelly1102 <58100933+tdonnelly1102@users.noreply.github.com>
2024-10-01 21:21:45 +0000

    created the class object and main method

--- MiddleThree.java
+++ MiddleThree.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class MiddleThree{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = keyboard.nextLine();
+        System.out.print("Enter a string with odd number of letters: ");
+        String str1 = keyboard.nextLine();
 
-        System.out.println("Hello, " + name);
+        int len = str1.length();
+        int mid = len/2;
+
+        String middleThree = str1.substring(mid-1, mid+2);
+        System.out.println("Middle three letters are " + middleThree);
+        
     }
 }
